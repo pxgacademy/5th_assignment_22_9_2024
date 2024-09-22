e0eefa553c12d5fe3841194799a4fd091bb141f0
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-09-22 22:35:26 +0600

    Completed history section

---
+++ js/tailwind.js
@@ -0,0 +1,15 @@
+// Tailwindcss cdn
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        primaryColor: "#B4F461",
+        hoverColor: "#99e23a",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+};
